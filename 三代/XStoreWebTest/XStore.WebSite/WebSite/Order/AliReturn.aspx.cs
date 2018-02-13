4f5c703ffe252f384c4809a6d54933a8e5d8645e
Se1fer <yyp673270171@sina.com>
2018-02-13 22:03:35 +0800

    阶段版本

---
+++ 三代/XStoreWebTest/XStore.WebSite/WebSite/Order/AliReturn.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace XStore.WebSite.WebSite.Order
+{
+    public partial class AliReturn : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
